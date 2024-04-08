f3e8645e577cc9fe0d50ea3b0ce1890378f8afcb
junmoxiaoDake <1369853821@qq.com>
2024-04-08 19:23:55 +0800

    nmmo_large

---
+++ nmmo_large/resource/maps/tiles.tsx
@@ -0,0 +1,34 @@
+// <?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiles" tilewidth="128" tileheight="128" tilecount="6" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1">
+  <image width="128" height="128" source="../tiles/lava.png"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="128" source="../tiles/water.png"/>
+ </tile>
+ <tile id="3">
+  <image width="128" height="128" source="../tiles/grass.png"/>
+ </tile>
+ <tile id="4">
+  <image width="128" height="128" source="../tiles/scrub.png"/>
+ </tile>
+ <tile id="5">
+  <image width="128" height="128" source="../tiles/forest.png"/>
+ </tile>
+ <tile id="6">
+  <image width="128" height="128" source="../tiles/stone.png"/>
+ </tile>
+  <tile id="7">
+  <image width="128" height="128" source="../tiles/farmland.png"/>
+ </tile>
+ <tile id="8">
+  <image width="128" height="128" source="../tiles/river.png"/>
+ </tile>
+ <tile id="9">
+  <image width="128" height="128" source="../tiles/mountains.png"/>
+ </tile>
+ <tile id="10">
+  <image width="128" height="128" source="../tiles/wasteland.png"/>
+ </tile>
+</tileset>
